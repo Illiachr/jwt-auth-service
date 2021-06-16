158038db07af30493441e2e5f9cc766cf46637b9
Illiachr <74302872+Illiachr@users.noreply.github.com>
2021-06-16 11:40:01 +0300

    implemented login, logout, refresh

--- modules/services/token.service.js
+++ modules/services/token.service.js
@@ -5,9 +5,27 @@ const tokenModel = require('../models/token.model');
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' });
-    const refreshToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '8h' });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '8h' });
 
     return { accessToken, refreshToken };
+  }
+
+  validateAccessToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+      return userData;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  validateRefreshToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+      return userData;
+    } catch (err) {
+      return null;
+    }
   }
 
   async saveToken(userId, refreshToken) {
@@ -20,6 +38,16 @@ class TokenService {
     const token = await tokenModel.create({ user: userId, refreshToken });
 
     return token;
+  }
+
+  async removeToken(refreshToken) {
+    const tokenData = await tokenModel.deleteOne({ refreshToken });
+    return tokenData;
+  }
+
+  async findToken(refreshToken) {
+    const tokenData = await tokenModel.findOne({ refreshToken });
+    return tokenData;
   }
 }
 
